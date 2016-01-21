9e57f556cfdca4c7cc135e1b1cd3c5103714cd6d
Chris Shmerling <cshmerli@ucsd.edu>
2016-01-21 18:25:53 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,41 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Holding on through the night");
+		$(".jumbotron p").toggleClass("active");
+
+		$("#submitBtn").click(updateProject);
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+function updateProject(e) {
+   var id = $('#project').val();
+   $(id).animate({
+      width: $('#width').val()
+   });
+
+   var newText = $('#description').val();
+   $(id + " .project-description").text(newText);
+}
+
+function projectClick(e){
+	console.log("Project clicked");
+	//prevent page from reloading
+	e.preventDefault();
+
+	var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(".project-description").fadeOut();
+    }
 }
